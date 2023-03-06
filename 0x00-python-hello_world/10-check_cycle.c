a8dc9f73864f88b11d8a2e329343827a8d7b8a41
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-06 14:28:36 +0300

    documntation added

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a list has a cycle
+ *
+ * @list: pointer to head of list
+ *
+ * Return: 0 -> if there is no cycle, 1 -> if there is a cycle.
+ */
+
+int check_cycle(listint_t *list)
+{
+}
