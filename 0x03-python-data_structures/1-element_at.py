3b7f52666b5c94d9eedefc5a1e55354fcebd84cd
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-13 09:57:04 +0300

    minor fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > len(my_list) or idx < 0:
+    if (idx < 0) or (idx > len(my_list) - 1):
         return None
     else:
         return my_list[idx]
