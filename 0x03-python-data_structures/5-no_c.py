e513d0464745015ae262b769c9a9018021752d16
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-13 10:34:00 +0300

    minor change

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    wthout_c = [x for x in my_string if x != 'c' or x != 'C']
-    return("".join(wthout_c))
+    without_c = [x for x in my_string if x != 'c' or x != 'C']
+    return ("".join(without_c))
