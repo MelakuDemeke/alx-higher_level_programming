aadfb95add7b39978f55c38110ba0b2c37e9707f
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-13 10:59:05 +0300

    minor fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
-    for i in my_list:
-        if i > max:
-            max = i
-    return max
+    if my_list:
+        max = 0
+        for i in my_list:
+            if i > max:
+                max = i
+        return max
+    return None
