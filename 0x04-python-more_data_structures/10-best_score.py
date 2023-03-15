f3bc3f3ea94b5a67658f8dbfb4c2da338e551b38
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-15 23:00:18 +0300

    minor fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(my_dict):
     if my_dict is None or my_dict == {}:
         return None
-    hig = max(my_dict.values())
+    high = max(my_dict.values())
     for key, value in my_dict.items():
-        if value is biggest:
+        if value is high:
             return key
