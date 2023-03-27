37b978c4f748b468f53ca25fc72aa0745ee76d34
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-27 11:15:08 +0300

    pycode style fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return(True)
+        return (True)
     except (ValueError, ValueError):
-        return(False)
+        return (False)
