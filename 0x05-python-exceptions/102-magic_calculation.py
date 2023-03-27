9ef6f73257036bc1a50a4d59b344d9126cb96d4d
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-27 14:12:11 +0300

    add else tag

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -5,7 +5,8 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception('Too far')
-            result += a ** b / i
+            else:
+                result += a ** b / i
         except Exception:
             result = b + a
             break
