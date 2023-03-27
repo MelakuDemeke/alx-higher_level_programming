474481ded39ffbaca5cab75d66f7f2565d691245
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-27 11:13:53 +0300

    pycode style fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,9 @@ def safe_print_list(my_list=[], x=0):
     printed = 0
     for i in range(x):
         try:
-            print(my_list[i],end="")
+            print(my_list[i], end="")
             printed += 1
         except IndexError:
             break
     print()
-    return(printed)
+    return (printed)
