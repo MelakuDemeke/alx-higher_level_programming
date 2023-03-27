15ada8af03d89915cce66cf4a5d2b8366eeff93e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-27 11:29:54 +0300

    fix to use .format()

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@ def safe_print_list_integers(my_list=[], x=0):
     printed = 0
     for i in range(x):
         try:
-            print(f"{my_list[i]:d}", end="")
+            print("{:d}".format(my_list[i]), end="")
             printed += 1
         except (ValueError, TypeError):
             continue
