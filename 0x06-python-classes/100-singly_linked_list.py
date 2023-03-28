80103750684a047a39875412e1ce096f04a7d9ef
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-28 15:08:22 +0300

    node completed

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,21 +1,36 @@
+#!/usr/bin/python3
+""" classes for a singly linked list."""
+
+
 class Node:
-    """represent node of singly linked list"""
+    """Represent a node in a singly-linked list."""
 
     def __init__(self, data, next_node=None):
-        """ Initialize a new node
-        Args:
-            data(int): data for new node
-            next_node(Node): pointer to next node
-        """
+        """Initialize a new Node.
 
-        self.__data = data
-        self.__next_node = next_node
+        Args:
+            data (int): The data of the new Node.
+            next_node (Node): The next node of the new Node.
+        """
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
-        return self.__data
+        return (self.__data)
 
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return (self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
