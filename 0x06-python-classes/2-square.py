c84cb04a33a8f8589f94eb2b6d54f0e9445099ef
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-28 13:54:24 +0300

    sqare with value validator

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Class to define square """
+
+
+class Square:
+    """ Represent a square """
+    def __init__(self, size):
+        """ Square initializer.
+        Args:
+            size (int): The size of the square
+          """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
