66a1cbc05b55fff6b956c0d36694647ca5f13613
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-28 13:45:04 +0300

    class with private size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Class to define square """
+
+
+class Square:
+    """ Represent a square """
+    def __init__(self, size):
+        """ Square initializer.
+        Args:
+            size (int): The size of the square
+          """
+        self.__size = size
