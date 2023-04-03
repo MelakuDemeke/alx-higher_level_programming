87907ebffa58420e93c519dc2e205eff21a8e677
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-03 12:40:20 +0300

    init board

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -6,3 +6,9 @@ Attributes:
     board (list): A list of list represent the chase board
     solution (list): a list of list containg solutions
 """
+import sys
+
+def init_board(n):
+    """initialize an n x n sized chaseboard with 0's """
+    pass
+
