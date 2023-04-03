71fb47e7ffecd095e4d4d0ddb1409bf4d158be05
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-03 12:38:36 +0300

    doc added

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+"""Solve the N-queens puzzle
+Determine all posible soluton to place N
+
+Attributes:
+    board (list): A list of list represent the chase board
+    solution (list): a list of list containg solutions
+"""
