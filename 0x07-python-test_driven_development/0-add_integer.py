4f670130e7a77844351721aa9ad6a7febaab1e25
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-03 09:52:25 +0300

    pycode style fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,8 +7,8 @@ def add_integer(a, b=98):
     Raises:
         TypeError: if either of a or b is a non-int and non-float
     """
-    if((not isinstance(a, int) and not isinstance(a, float))):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if((not isinstance(b, int) and not isinstance(b, float))):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    return(int(a) + int(b))
+    return (int(a) + int(b))
