1efcf805f2e9f7795e58a59d0b6b53545a23143e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-04 15:36:59 +0300

    test case written

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Unittests for max_integer([..])."""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Define unittests for max_integer([..])."""
+
+    def test_ordered_list(self):
+        """orderd list int test"""
+        ordered = [1, 2, 3, 4]
+        self.assertEqual(max_integer(ordered), 4)
+
+    def test_unordered_list(self):
+        """unorderd list test"""
+        unordered = [1, 2, 4, 3]
+        self.assertEqual(max_integer(unordered), 4)
+
+    def test_max_at_begginning(self):
+        """test with initial max value"""
+        max_at_beginning = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_at_beginning), 4)
+
+    def test_empty_list(self):
+        """Empty list test"""
+        empty = []
+        self.assertEqual(max_integer(empty), None)
+
+    def test_one_element_list(self):
+        """single element test"""
+        one_element = [7]
+        self.assertEqual(max_integer(one_element), 7)
+
+    def test_floats(self):
+        """floats test"""
+        floats = [1.53, 6.33, -9.123, 15.2, 6.0]
+        self.assertEqual(max_integer(floats), 15.2)
+
+    def test_ints_and_floats(self):
+        """test with int anf float combination"""
+        ints_and_floats = [1.53, 15.5, -9, 15, 6]
+        self.assertEqual(max_integer(ints_and_floats), 15.5)
+
+    def test_string(self):
+        """String test"""
+        string = "Melaku"
+        self.assertEqual(max_integer(string), 'r')
+
+    def test_list_of_strings(self):
+        """test with multi string"""
+        strings = ["Melaku", "Demeke", "is", "amazing"]
+        self.assertEqual(max_integer(strings), "name")
+
+    def test_empty_string(self):
+        """empty string test"""
+        self.assertEqual(max_integer(""), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
