9e54a19371432ea416f13b4927ce3b06676193c3
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-10 14:42:41 +0300

    area not em

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    def area(self):
+        "area which will be implemented"
+        raise Exception("area() is not implemented")
