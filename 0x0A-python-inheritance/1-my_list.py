75303110e12aedfff525b9e5c354f680fd728fc5
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-10 14:12:39 +0300

    change test case

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,9 @@
 
 class MyList(list):
     """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
     def print_sorted(self):
         """prints the sorted list"""
