a12c38f6a76cde085540068366d55f87072b9429
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-10 14:35:31 +0300

    inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines an inherited class-checking function."""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+
+    Returns:
+        True if obj is an inherited instance of a_class, otherwise False.
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
