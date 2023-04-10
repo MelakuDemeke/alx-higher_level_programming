768a5e0399b591764ba919301159bb319ede58a0
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-10 14:16:26 +0300

    fix main code

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""MyList class that drives from list"""
+"""Defines derived class MyList from list"""
 
 
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
+    """drived class"""
 
     def print_sorted(self):
-        """prints the sorted list"""
+        """Print a list in sorted ascending order"""
         print(sorted(self))
