7ba15e3cca95c380b9a208f5ce1290d8b4adcdbb
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-10 14:00:47 +0300

    remove init

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,9 +4,6 @@
 
 class MyList(list):
     """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
 
     def print_sorted(self):
         """prints the sorted list"""
