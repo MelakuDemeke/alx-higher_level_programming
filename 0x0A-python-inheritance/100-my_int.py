e8450a7e95622df85584e387d4d665c0045a7d37
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-10 15:37:08 +0300

    change place

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """define class MyInt that inherits from int"""
 
+
 class MyInt(int):
     """invert int operatior != and =="""
 
-    def __ne__(self, value):
-        "change != to =="
-        return self.real == value
-
     def __eq__(self, value):
+        return self.real != value
+
+    def __ne__(self, value):
         return self.real == value
