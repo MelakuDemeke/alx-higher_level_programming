1ad2daf10745233e66fd9e949f07384cc34543b3
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-11 17:04:50 +0300

    add exeptions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""This module adds all arguments to a Python list and save them to a file."""
+"""
+add all arguments to a Python list
+"""
+import json
+from sys import argv
 
 
-import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+try:
+    list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    list = []
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+for arg in argv[1:]:
+    list.append(arg)
+
+save_to_json_file(list, "add_item.json")
