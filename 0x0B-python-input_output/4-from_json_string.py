408ce6f93bf16824d9af2014899b627b8ea4a98d
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-11 16:08:40 +0300

    json to obj

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 6-from_json_string.py
+"""JSON-to-object"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Converts a JSON string to its corresponding Python object.
+
+    Args:
+        json_string (str): The JSON string to be converted.
+
+    Returns:
+        object: The Python object representation of the JSON string.
+    """
+    return json.loads(my_str)
