53f7d6c857d2d7fd1a78020f0bc3c6b44d33deb2
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-11 16:55:33 +0300

    stat update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,80 +1,35 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
+"""Reads from standard input and computes metrics."""
 
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
-"""
-
-
-from collections import defaultdict
 import sys
 
-def print_stats(size, status_codes):
-    """Print accumulated metrics.
+size = 0
+status_codes = {}
+valid_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
+count = 0
 
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
-    """
+try:
+    for line in sys.stdin:
+        count += 1
+        try:
+            code, content = line.split()[-2:]
+            if code in valid_codes:
+                status_codes[code] = status_codes.get(code, 0) + 1
+            size += int(content)
+        except ValueError:
+            pass
+        if count == 10:
+            print("File size: {}".format(size))
+            for key in sorted(status_codes):
+                print("{}: {}".format(key, status_codes[key]))
+            count = 0
+    if count > 0:
+        print("File size: {}".format(size))
+        for key in sorted(status_codes):
+            print("{}: {}".format(key, status_codes[key]))
+
+except KeyboardInterrupt:
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
-
-
-def handle_code_status(line, status_codes, valid_codes):
-    """Handle status code in a line.
-
-    Args:
-        line (str): A line of input.
-        status_codes (dict): The accumulated count of status codes.
-        valid_codes (list): A list of valid status codes.
-    """
-    try:
-        code = line.split()[-2]
-        if code in valid_codes:
-            status_codes[code] += 1
-    except IndexError:
-        pass
-
-
-def process_line(line, count, size, status_codes, valid_codes):
-    """Process a line of input.
-
-    Args:
-        line (str): A line of input.
-        count (int): The number of lines processed so far.
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
-        valid_codes (list): A list of valid status codes.
-    Returns:
-        int: The updated line count.
-    """
-    try:
-        size += int(line.split()[-1])
-    except (IndexError, ValueError):
-        pass
-
-    handle_code_status(line, status_codes, valid_codes)
-
-    if count == 10:
-        print_stats(size, status_codes)
-        count = 0
-
-    return count + 1
-
-
-if __name__ == "__main__":
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
-    size = 0
-    status_codes = defaultdict(int)
-
-    try:
-        for count, line in enumerate(sys.stdin, start=1):
-            count = process_line(line, count, size, status_codes, valid_codes)
-        print_stats(size, status_codes)
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+    raise
