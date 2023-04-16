4ec00b8816589bdbdfc9177364b0e2e626c16e35
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-16 20:52:50 +0300

    type check add for height

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -32,6 +32,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
     
     @property
