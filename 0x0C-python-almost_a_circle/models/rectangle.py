a73e8b3cf2021616d0e017275f50ddf36c9c4e93
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-16 20:53:16 +0300

    type check added for x

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -44,6 +44,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value <= 0:
+            raise ValueError("x must be > 0")
         self.__x = value
     
     @property
