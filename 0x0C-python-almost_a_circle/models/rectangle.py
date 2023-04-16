a4517914c4e3065ba14cfcc60bd97bec94958f50
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-16 21:09:44 +0300

    equal removed for x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,6 @@
-#!/usr/bin/pyton3
-from models.base import Base
+#!/usr/bin/python3
 """define rectagle class"""
-
+from models.base import Base
 
 class Rectangle(Base):
     """Represent a new rectangle"""
@@ -57,8 +56,8 @@ class Rectangle(Base):
     def x(self, value):
         if type(value) != int:
             raise TypeError("x must be an integer")
-        if value <= 0:
-            raise ValueError("x must be > 0")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -69,6 +68,6 @@ class Rectangle(Base):
     def y(self, value):
         if type(value) != int:
             raise TypeError("y must be an integer")
-        if value <= 0:
-            raise ValueError("y must be > 0")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
