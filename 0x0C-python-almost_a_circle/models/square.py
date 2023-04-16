53895c1135c04cf26f43a962db4faa6d4e675f60
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-16 21:43:48 +0300

    square defined

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a new  square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square
+
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
