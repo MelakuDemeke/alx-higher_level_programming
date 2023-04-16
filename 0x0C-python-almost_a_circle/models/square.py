e34acf945a7f449ed5dffa39be085d94812af796
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-16 21:59:48 +0300

    dictionary representation of the Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -45,6 +45,15 @@ class Square(Rectangle):
             for k, v in kwargs.items():
                 setattr(self, k, v)
 
+    def to_dictionary(self):
+        """Return the dictionary representation of the Square"""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
+
     def __str__(self):
         """Return the print() and str() representation of a Square"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
