ebb1e7a0ca0d0255e794ef719cfab3f47ac884cd
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-16 20:53:53 +0300

    value check for y added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -56,4 +56,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value <= 0:
+            raise ValueError("y must be > 0")
         self.__y = value
