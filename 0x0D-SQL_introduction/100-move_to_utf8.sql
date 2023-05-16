71058e1b87272f242b42de817f4505ba4a7c58bd
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-16 19:26:03 +0300

    minor change

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,5 @@
 -- Convert the hbtn_0c_0 database to utf8mb4 character set
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0
 
 -- Convert the first_table table to utf8mb4 character set
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the name field in the first_table table to utf8mb4 character set
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT 'Name of the person';
