1e664100bc3199e2c02c930650952898842add86
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-16 19:30:45 +0300

    remove the conversion

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,8 +1,8 @@
--- Select the city and calculate the average temperature in Fahrenheit, rounding to 2 decimal places
-SELECT city, ROUND(AVG((temperature * 9/5) + 32), 2) AS avg_temp_f
+-- Select the city and calculate the average temperature
+SELECT city, AVG('value') AS avg_temp
 -- From the weather_data table
 FROM weather_data
 -- Group the results by city
 GROUP BY city
 -- Order the results by average temperature in Fahrenheit, descending
-ORDER BY avg_temp_f DESC;
+ORDER BY avg_temp DESC;
