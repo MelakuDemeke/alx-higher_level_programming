b40202138530e87eb901386631944d73ec826ad5
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-17 20:59:05 +0300

    ahhh

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -7,4 +7,4 @@ SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
        LEFT JOIN `tv_show_genres` AS g
        ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`;ORDER BY tv_shows.title ASC, genre_id;
+ ORDER BY s.`title`, g.`genre_id`;
