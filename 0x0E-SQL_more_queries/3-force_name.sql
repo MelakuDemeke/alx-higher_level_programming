9b3613d85502266a1b5e71dced115417c70ad790
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-17 20:09:45 +0300

    remove extra coma

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS `force_name` (
   -- Define the id column as an integer
   `id` INT,
   -- Define the name column as a non-null string
-  `name` VARCHAR(256) NOT NULL,
+  `name` VARCHAR(256) NOT NULL
   -- Define the id column as the primary key of the table
 );
