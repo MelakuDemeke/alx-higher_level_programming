a2a4023e03ae21f9d083da30e06fb5ff6be0aa32
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-17 20:56:51 +0300

    add left join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,8 +3,8 @@
 -- Use IFNULL to display NULL if a TV show doesn't have a genre
 -- Order the results in ascending order by the TV show title and genre ID
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, genre_id;
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;ORDER BY tv_shows.title ASC, genre_id;
