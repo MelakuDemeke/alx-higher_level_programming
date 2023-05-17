05b48059212593f8d04473c6320684b479528970
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-17 21:06:20 +0300

    explain the step

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Select the genre and count the number of TV shows with that genre
+-- Join the tv_shows and tv_show_genres tables to get the genre for each TV show
+-- Only count shows that have a genre linked (i.e. where genre_id is not NULL)
+-- Group the results by genre
+-- Only show genres that have at least one linked TV show
+-- Order the results in descending order by the number of shows linked
