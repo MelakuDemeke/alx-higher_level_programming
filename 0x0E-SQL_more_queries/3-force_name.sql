4fedf24c52741be04233f8b5ea085d7e932334fe
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-17 20:10:11 +0300

    fix

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -4,5 +4,4 @@ CREATE TABLE IF NOT EXISTS `force_name` (
   `id` INT,
   -- Define the name column as a non-null string
   `name` VARCHAR(256) NOT NULL
-  -- Define the id column as the primary key of the table
 );
