f87fcc221a5ac94ef4854f9bba10ba2a92c8d4e2
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-17 21:14:13 +0300

    list all comedy shows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,12 @@
+-- Select the titles of all Comedy shows
+-- Join the tv_shows and tv_show_genres tables to get the shows with the Comedy genre
+-- Only select shows with the Comedy genre
+-- Order the results in ascending order by the show title
+SELECT t.`title`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
+ ORDER BY t.`title`;
