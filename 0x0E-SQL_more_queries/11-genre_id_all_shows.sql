546e98de92ef3860bf706aff1433f27443373f89
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-17 20:56:05 +0300

    remove unwanted

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,8 +3,8 @@
 -- Use IFNULL to display NULL if a TV show doesn't have a genre
 -- Order the results in ascending order by the TV show title and genre ID
 
-SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, genre_id ASC;
+ORDER BY tv_shows.title ASC, genre_id;
