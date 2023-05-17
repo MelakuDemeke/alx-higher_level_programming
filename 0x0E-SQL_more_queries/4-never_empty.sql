b0ac418a2aced1b0e39ad4890ce555a294ef44bf
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-17 20:20:15 +0300

    another one

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,10 @@
+-- Use the database_name database
+USE `database_name`;
+
+-- Create the id_not_null table if it doesn't exist already
+CREATE TABLE IF NOT EXISTS `id_not_null` (
+  -- Define the id column as an integer with a default value of 1
+  `id` INT DEFAULT 1,
+  -- Define the name column as a string
+  `name` VARCHAR(256)
+);
