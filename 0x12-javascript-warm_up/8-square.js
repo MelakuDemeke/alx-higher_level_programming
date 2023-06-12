fdf935c6edb033840bc9a7da5303b8225071983b
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-12 14:29:09 +0300

    re arrange the code

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,14 +2,12 @@
 
 const size = Math.floor(Number(process.argv[2]));
 
-if (!isNaN(size)) {
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
   for (let i = 0; i < size; i++) {
     let row = '';
-    for (let j = 0; j < size; j++) {
-      row += 'X'
-    }
+    for (let j = 0; j < size; j++) row += 'X';
     console.log(row);
   }
-} else {
-  console.log('Missing size');
 }
