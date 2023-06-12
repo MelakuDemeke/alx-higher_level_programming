d70f3ca4d9820e5c4c2f3c9dcb29aff8c9347815
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-12 14:38:23 +0300

    remove the num checker

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 
-const firstNum = Math.floor(Number(process.argv[2]));
-const secondNum = Math.floor(Number(process.argv[3]));
+const firstNum = (Number(process.argv[2]));
+const secondNum = (Number(process.argv[3]));
 
-if (isNaN(firstNum) || isNaN(secondNum)) {
-  console.log('NaN');
-} else {
-  console.log(add(firstNum, secondNum));
-}
+console.log(add(firstNum, secondNum));
+
 
 function add(a, b){
   return a + b;
