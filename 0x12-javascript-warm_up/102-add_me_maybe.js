f768d139c642adb2a3d2f90d4f2c0e591fbe03b9
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-12 15:49:54 +0300

    change name

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-exports.addMeMaybe = function (number, thefunction) {
-  thefunction(++number);
-}
+exports.addMeMaybe = function (number, theFunction) {
+    theFunction(++number);
+  };
