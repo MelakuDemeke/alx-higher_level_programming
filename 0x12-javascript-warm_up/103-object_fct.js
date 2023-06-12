5a7685cc65eb18e63b14ea6a3280a0cfe2ffe5f4
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-12 15:59:45 +0300

    takeout the inc obj

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function () {
-    this.value++;
-  }
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
 };
 
-console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
