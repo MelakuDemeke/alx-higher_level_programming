ef16a62fed0ef6c0c07d0b058643728b2d6b1ae1
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-12 14:40:40 +0300

    move the function up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
+function add (a, b) {
+  return a + b;
+}
+
 const firstNum = (Number(process.argv[2]));
 const secondNum = (Number(process.argv[3]));
 
 console.log(add(firstNum, secondNum));
-
-
-function add(a, b) {
-  return a + b;
-}
