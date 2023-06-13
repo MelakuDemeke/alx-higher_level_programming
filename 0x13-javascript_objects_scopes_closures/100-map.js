2706d063328e52481c2de7e706fc99610bdbe871
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-13 15:10:38 +0300

    update the log

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 
-const list = require('./100-data.js').list;
+const { list } = require('./100-data');
+
+const newList = list.map((value, index) => value * index);
+
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(newList);
