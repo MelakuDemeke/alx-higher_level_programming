c48aa5b583b77a95199fa43f0d158d3ed6038467
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-13 14:36:09 +0300

    minor fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -11,7 +11,7 @@ class Rectangle {
   print () {
     for (let i = 0; i < this.height; i++) {
       let row = '';
-      for (let j = 0; j < this.width; i++) {
+      for (let j = 0; j < this.width; j++) {
         row += 'X';
       }
       console.log(row);
