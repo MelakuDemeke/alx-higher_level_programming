39bcbe3bf6a9f3c27426adb3bcc19c188866e31e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-13 10:23:36 +0300

    print module added

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    for (i = 0; i < this.height; i++){
+      let row = '';
+      for (j = 0; j < this.width; i++){
+        row += '#'
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
