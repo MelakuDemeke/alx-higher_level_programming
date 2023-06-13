acb32583f91fb43ccabc1df9f410e63c3fc63ea2
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-13 10:30:34 +0300

    double and rotate added

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (i = 0; i < this.height; i++){
+      let row = '';
+      for (j = 0; j < this.width; i++){
+        row += '#'
+      }
+    }
+  }
+
+  rotate () {
+    let temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height *2;
+  }
+}
+
+module.exports = Rectangle;
