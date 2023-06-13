5dfbd1dfde968184ec02dbe2d6f75d773b1af1b8
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-13 10:49:49 +0300

    square defined

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const SquareP = require('./5-square');
+
+class Square extends SquareP {
+	charPrint (c) {
+		if (c === undefined){
+			c ='X';
+		}
+		for (let i = 0; i < this.height; i++) {
+			let row = '';
+			for (let j = 0; j < this.width; j++) {
+				row += c;
+			}
+			console.log(row);
+		}
+	}
+}
+
+module.exports = Square;
