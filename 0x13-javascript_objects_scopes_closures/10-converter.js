658e58c43aeac74f02322650f4cfb08307f4f617
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-13 15:04:05 +0300

    return a function

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  const convertToBase = num => num.toString(base);
-  return convertToBase;
+  return function (num) {
+    return num.toString(base);
+  };
 };
