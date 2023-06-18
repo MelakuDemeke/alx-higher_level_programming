a30d2681cd9a6b1ce8572ef45bc4b5bc5422ca6f
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-18 21:36:15 +0300

    query and print with provided name with associated city

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This script retrieves and prints the State object with the provided name
+from the database, along with the associated cities, in a different format.
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
+if __name__ == "__main__":
+    db_username = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    uri = 'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}'
+    db_url = uri.format(
+        username=db_username,
+        password=db_password,
+        dbname=db_name
+    )
+
+    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+        for city_ins in instance.cities:
+            print(city_ins.id, city_ins.name, sep=": ", end="")
+            print(" -> " + instance.name)
