4a68ab2ae49df0275e4347846b0cfc458094b324
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-18 21:09:35 +0300

    state objct print

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This script retrieves and prints the State object
+from the database based on the provided name.
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    db_username = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    uri = 'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}'
+    db_url = uri.format(
+        username=db_username,
+        password=db_password,
+        dbname=db_name
+    )
+
+    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for instance in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
