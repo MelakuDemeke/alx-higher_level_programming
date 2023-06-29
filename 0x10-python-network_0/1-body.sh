3ee115c6a65bf8f444832d83c45d759c0b02eda3
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-29 20:57:50 +0300

    change comments

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request to the URL,
-# and displays the body of the response
+# Get the response body for a given URL for 200 status code responses.
 curl -sL "$1"
