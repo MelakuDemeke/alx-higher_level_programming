14694dafb1844a9b65b10382bf4356bde071dafe
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-30 10:33:03 +0300

    fix comment

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a DELETE request to a given URL and display the response body.
+# Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
 curl -sX DELETE "$1"
