57779aac9263c198fda69eeeb861e78b0da96fbb
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-30 16:09:27 +0300

    dich try block

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,9 +7,8 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        print(response.text)
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e))
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
