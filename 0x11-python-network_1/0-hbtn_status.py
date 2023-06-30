6a2c4db153951bd78f7260a9efe9912aee08b83b
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-30 14:45:25 +0300

    requesto to intranet

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status """
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
