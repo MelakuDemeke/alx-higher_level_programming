2351be372a3cf26c825bbf41ba3b8c927d4fd9a3
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-30 10:33:51 +0300

    fix comment

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+# Bash script that takes in a URL and displays all HTTP methods the server will accept
 curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
