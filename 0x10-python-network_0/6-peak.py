7a272a19df61317a48ad65ae49b40b5a8f5404cc
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-30 11:48:34 +0300

    full functionality

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -16,6 +16,20 @@ def find_peak(list_of_integers):
     # If the list is empty, return None.
     if not list_of_integers:
         return None
-    
-    
+
+    length = len(list_of_integers)
+    mid = length // 2
+    arr = list_of_integers
+
+    # Check if the middle element is a peak
+    if (mid == 0 or arr[mid - 1] <= arr[mid]) and \
+            (mid == length - 1 or arr[mid + 1] <= arr[mid]):
+        return arr[mid]
+
+    # If the right neighbor is greater, search in the right subarray
+    if mid < length - 1 and arr[mid + 1] > arr[mid]:
+        return find_peak(arr[mid + 1:])
+
+    # If the left neighbor is greater, search in the left subarray
+    return find_peak(arr[:mid])
     pass
