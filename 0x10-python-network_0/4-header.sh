73f33ab96b7215a76f38b0559b105931733996a2
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-30 10:31:23 +0300

    fix comment

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
+# Bash script that takes in a URL and displays all HTTP methods the server will accept.
 curl -sH "X-School-User-Id: 98" "${1}"
