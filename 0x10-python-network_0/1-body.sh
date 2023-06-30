3fc4ccaf59295ed6692cb617953363772f800976
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-30 10:26:37 +0300

    fix comment

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
+# Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sL "$1"
