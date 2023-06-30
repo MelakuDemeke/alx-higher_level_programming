753b56c8c32117f159df608704b39b1a3e377eb2
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-30 11:46:08 +0300

    add doc

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,4 +3,19 @@
 # a function that finds a peak in a list of unsorted integers.
 
 def find_peak(list_of_integers):
+    """
+    Find a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): The list of unsorted integers.
+
+    Returns:
+        int or None: The peak element if found, or None if the list is empty.
+
+    """
+    # If the list is empty, return None.
+    if not list_of_integers:
+        return None
+    
+    
     pass
