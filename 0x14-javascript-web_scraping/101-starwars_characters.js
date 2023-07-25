913e2a99d576c707e230286be1f13d8a2b9ef3ca
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-25 12:21:21 +0300

    re make teh code

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,28 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
+  if (!error) {
     const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, function (error, response, body) {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    printCharacters(characters, 0);
+  } else {
+    console.error(error);
   }
+});
+
+function printCharacters(characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      printCharacters(characters, index + 1);
+    } else {
+      console.error(error);
+    }
+  });
 }
-);
