678831923b294c9bcfc16cfff5fa14c924db87d5
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-25 11:37:47 +0300

    count wedge apearance

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+    if (error) {
+        console.error(error);
+    } else {
+        const films = JSON.parse(body).results;
+        let count = 0;
+        for (const film of films) {
+            for (const character of film.characters) {
+                if (character.endsWith('/18/')) {
+                    count++;
+                }
+            }
+        }
+        console.log(count);
+    }
+}
+);
