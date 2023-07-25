76b958bb41e2aba6acc89e20ac2e51a39a2311bd
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-25 12:07:11 +0300

    fix semi standard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,10 +3,10 @@
 const url = process.argv[2];
 const request = require('request');
 request(url, function (error, response) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log('code: ' + response.statusCode);
-    }
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 }
 );
