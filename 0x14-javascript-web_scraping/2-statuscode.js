2cb1a776e030ef24fd0d45976bd438078ca0e189
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-25 11:12:13 +0300

    ignore node packages

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+request(url, function (error, response) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log('code: ' + response);
+    }
+}
+);
