40c9be187080f2c27ef9a7f60d5aa9892d358aba
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-25 12:00:11 +0300

    star war characters

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
+
+request(url, function (error, response, body) {
+    if (error) {
+        console.error(error);
+    } else {
+        const characters = JSON.parse(body).characters;
+        for (const character of characters) {
+            request(character, function (error, response, body) {
+                if (error) {
+                    console.error(error);
+                } else {
+                    console.log(JSON.parse(body).name);
+                }
+            });
+        }
+    }
+}
+);
