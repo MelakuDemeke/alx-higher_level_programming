28125e0c30d22c38b24e467b7697e722a1b86a0f
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-25 12:14:16 +0300

    fix semi standard

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,20 +5,19 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
 
 request(url, function (error, response, body) {
-    if (error) {
-        console.error(error);
-    } else {
-        const characters = JSON.parse(body).characters;
-        for (const character of characters) {
-            request(character, function (error, response, body) {
-                if (error) {
-                    console.error(error);
-                } else {
-                    console.log(JSON.parse(body).name);
-                }
-            });
+  if (error) {
+    console.error(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      request(character, function (error, response, body) {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(JSON.parse(body).name);
         }
+      });
     }
+  }
 }
 );
-
