d7e985ce027a2d08c4d3d180daeec72ff6b37ac8
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-25 12:12:07 +0300

    semi standard fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
 
-url = process.argv[2];
-file = process.argv[3];
-
 const request = require('request');
 const fs = require('fs');
 
-request(url, function (error, response, body) {
-    if (error) {
-        console.error(error);
-    } else {
-        fs.writeFile(file, body, 'utf-8', (err) => {
-        if (err) {
-            console.error(err);
-        }
-        });
-    }
-    }
-);
+const url = process.argv[2];
+const file = process.argv[3];
 
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(file, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+}
+);
