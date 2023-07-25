58390d8145e9a7e295d9d00b19cab95dd5918e02
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-25 12:08:28 +0300

    fix semi standrd

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,19 +4,19 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
-    if (error) {
-        console.error(error);
-    } else {
-        const films = JSON.parse(body).results;
-        let count = 0;
-        for (const film of films) {
-            for (const character of film.characters) {
-                if (character.endsWith('/18/')) {
-                    count++;
-                }
-            }
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.endsWith('/18/')) {
+          count++;
         }
-        console.log(count);
+      }
     }
+    console.log(count);
+  }
 }
 );
