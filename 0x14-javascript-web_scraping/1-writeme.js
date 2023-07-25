bd2d7c259dc2d9a86ec4c5532266b0f7ad98b402
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-25 12:05:26 +0300

    fix semi standard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,7 +6,7 @@ const filePath = process.argv[2];
 const content = process.argv[3];
 
 fs.writeFile(filePath, content, 'utf-8', (err) => {
-    if (err) {
-        console.error(err);
-    }
+  if (err) {
+    console.error(err);
+  }
 });
