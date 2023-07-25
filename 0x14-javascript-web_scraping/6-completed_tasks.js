603a2cd752bf290c2740d09988675291f8e26897
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-25 12:13:02 +0300

    semi standard fixed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,24 @@
 #!/usr/bin/node
 
-url = process.argv[2];
 const request = require('request');
 
+const url = process.argv[2];
+
 request(url, function (error, response, body) {
-    if (error) {
-        console.error(error);
-    } else {
-        const todos = JSON.parse(body);
-        const completed = {};
-        for (const todo of todos) {
-            if (todo.completed) {
-                if (completed[todo.userId] === undefined) {
-                    completed[todo.userId] = 1;
-                } else {
-                    completed[todo.userId] += 1;
-                }
-            }
+  if (error) {
+    console.error(error);
+  } else {
+    const todos = JSON.parse(body);
+    const completed = {};
+    for (const todo of todos) {
+      if (todo.completed) {
+        if (completed[todo.userId] === undefined) {
+          completed[todo.userId] = 1;
+        } else {
+          completed[todo.userId] += 1;
         }
-        console.log(completed);
+      }
     }
+    console.log(completed);
+  }
 });
