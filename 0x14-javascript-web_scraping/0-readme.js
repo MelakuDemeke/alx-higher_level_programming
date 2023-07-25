8c83e2ac013d6df0270741403087229faf5f5eb5
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-25 12:04:22 +0300

    semi standard fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const file = process.argv[2];
 
 fs.readFile(file, 'utf8', function (err, data) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
